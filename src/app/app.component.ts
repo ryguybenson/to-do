ccd982b00a44c7f011f555451ec37d937385bc90
Nathan Nielson and Riley Benson <nathan+riley@>
2019-03-26 13:17:20 -0700

    update angular functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Component } from '@angular/core';
 import { Task } from './models/task.model';
 
 @Component({
@@ -13,9 +12,26 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+
+ priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+  selectedTask = null;
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+  this.selectedTask = null;
+  }
 }
